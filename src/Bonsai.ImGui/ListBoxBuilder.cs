19b0e59ae32b93eec7eb0efdd585fec0a3e9c023
glopesdev <g.lopes@neurogears.org>
2026-04-18 10:12:58 +0100

    Add list box and combo box controls (#4)
    Data is backed and edited using an array of string items for both list
    and combo variants.

---
+++ src/Bonsai.ImGui/ListBoxBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Bonsai.ImGui;
+using ImGui = Hexa.NET.ImGui.ImGui;
+
+/// <summary>
+/// Represents an operator that draws a list box control and generates
+/// a sequence of notifications whenever the selected item changes.
+/// </summary>
+[Description("Draws a list box control and generates a sequence of notifications whenever the selected item changes.")]
+public class ListBoxBuilder : ControlBuilderBase<string>
+{
+    /// <summary>
+    /// Gets or sets the available items to display in the list box.
+    /// </summary>
+    [Description("The available items to display in the list box.")]
+    public string[] Items { get; set; }
+
+    /// <inheritdoc/>
+    protected override IObservable<string> Generate<TSource>(IObservable<TSource> source)
+    {
+        return Observable.Create<string>(observer =>
+        {
+            var currentItem = 0;
+            var items = Items ?? [];
+            var label = $"##{Name ?? nameof(ImGui.ListBox)}";
+            var sourceObserver = Observer.Create<TSource>(
+                _ =>
+                {
+                    if (Visible && ImGui.ListBox(label, ref currentItem, items, items.Length))
+                        observer.OnNext(items[currentItem]);
+                },
+                observer.OnError,
+                observer.OnCompleted);
+            return source.SubscribeSafe(sourceObserver);
+        });
+    }
+}
